457af652cef4071929b3573e3336eca6a98a0723
waydaxp <waydaxp@gmail.com>
2025-06-30 02:36:28 +0800

    Update generate_html.py

--- generate_html.py
+++ generate_html.py
@@ -1,20 +1,12 @@
 # generate_html.py
-
-import os
-from datetime import datetime
-import pytz
 from generate_data import get_all_analysis
 
-TEMPLATE = open("index_template.html", encoding="utf-8").read()
-
 def main():
+    tpl = open("index_template.html", encoding="utf-8").read()
     ctx = get_all_analysis()
-    ctx["page_update_time"] = datetime.now(pytz.timezone("Asia/Shanghai")) \
-        .strftime("%Y-%m-%d %H:%M")
-    html = TEMPLATE.format(**ctx)
+    html = tpl.format(**ctx)
     with open("index.html", "w", encoding="utf-8") as f:
         f.write(html)
-    print("✅ index.html generated")
 
 if __name__ == "__main__":
     main()
