542f36db776cd64a8d15eb074ab1f2fdd23f4fb8
waydaxp <waydaxp@gmail.com>
2025-06-30 02:53:37 +0800

    Update generate_html.py

--- generate_html.py
+++ generate_html.py
@@ -1,12 +1,14 @@
-# generate_html.py
+import os
 from generate_data import get_all_analysis
 
+TEMPLATE = open("index_template.html", encoding="utf-8").read()
+
 def main():
-    tpl = open("index_template.html", encoding="utf-8").read()
-    ctx = get_all_analysis()
-    html = tpl.format(**ctx)
+    ctx  = get_all_analysis()
+    html = TEMPLATE.format(**ctx)
     with open("index.html", "w", encoding="utf-8") as f:
         f.write(html)
+    print("✅ index.html 已更新")
 
 if __name__ == "__main__":
     main()
