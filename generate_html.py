9b584d385d13f2a43d5f351426f3f02eea3a9ffa
waydaxp <waydaxp@gmail.com>
2025-06-30 03:03:19 +0800

    Update generate_html.py

--- generate_html.py
+++ generate_html.py
@@ -1,11 +1,12 @@
-import os
-from generate_data import get_all_analysis
+# generate_html.py
 
-TEMPLATE = open("index_template.html", encoding="utf-8").read()
+from generate_data import get_all_analysis
+from jinja2 import Template
 
 def main():
-    ctx  = get_all_analysis()
-    html = TEMPLATE.format(**ctx)
+    ctx = get_all_analysis()
+    tpl = open("index_template.html", "r", encoding="utf-8").read()
+    html = Template(tpl).render(**ctx)
     with open("index.html", "w", encoding="utf-8") as f:
         f.write(html)
     print("✅ index.html 已更新")
