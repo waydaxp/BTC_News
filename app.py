a0c270e58544e12c3edb6eff70190ce97aff6dec
waydaxp <waydaxp@gmail.com>
2025-07-07 02:04:35 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from generate_data import get_all_analysis
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    ctx = get_all_analysis()
+    return render_template('index.html', **ctx)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
