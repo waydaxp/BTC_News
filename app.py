c0a751acf20d44780a35f1d4e382e052d83adda8
waydaxp <waydaxp@gmail.com>
2025-07-08 02:27:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Flask 服务正常运行！"
+    return "Hello from Gunicorn + Flask!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
